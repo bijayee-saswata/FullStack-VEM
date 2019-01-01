3309b4f55f1dfbcb89086cc29f8cdb2b5859e784
bijayee-saswata <saswat.sipun@gmail.com>
2019-01-01 19:25:08 +0530

    added get,post and delete

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -21,7 +21,21 @@ router.get('/', async (req,res)=>{
 
 //Add posts
 
+router.post('/', async (req,res)=>{
+    const posts = await loadPosts();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+
 //Delete posts 
 
+router.delete('/:id', async (req,res)=>{
+    const posts = await loadPosts();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+})
 
 module.exports = router;

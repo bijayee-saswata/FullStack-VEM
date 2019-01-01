32c88c93914fa0940e5cc67cb129c55d07f73d32
bijayee-saswata <saswat.sipun@gmail.com>
2019-01-01 17:48:54 +0530

    added routing

--- server/index.js
+++ server/index.js
@@ -8,4 +8,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+const posts = require('./routes/api/posts')
+app.use('/api/posts', posts);
+
 const port = process.env.PORT || 5000;
+
+app.listen(port, () =>{
+    console.log('Server started at ' + port);
+    
+})
---
+++ server/routes/api/posts.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get posts
+
+router.get('/', (req,res)=>{
+    res.send('hello');
+})
+
+//Add posts
+
+//Delete posts 
+
+
+module.exports = router;

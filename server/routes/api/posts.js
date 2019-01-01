7d02b7aa2e71a650581cdab6334724a2b8af2b93
bijayee-saswata <saswat.sipun@gmail.com>
2019-01-01 18:24:57 +0530

    added mongodb database

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -3,10 +3,20 @@ const mongodb = require('mongodb');
 
 const router = express.Router();
 
+//mongodb connection
+async function loadPosts(){
+    const client = await mongodb.connect('mongodb://abc123:abc123@ds131983.mlab.com:31983/nodeblog', {
+        useNewUrlParser : true
+    });
+    return client.db('nodeblog').collection('posts');
+}
+
+
 //Get posts
 
-router.get('/', (req,res)=>{
-    res.send('hello');
+router.get('/', async (req,res)=>{
+    const posts = await loadPosts();
+    res.send(await posts.find({}).toArray());
 })
 
 //Add posts
